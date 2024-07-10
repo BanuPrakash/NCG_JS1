260627192bff76b912d6759cbd354e340f1d02ea
Banuprakash <banuprakashc@yahoo.co.in>
2024-07-10 12:41:52 +0530

    day 1

--- codes/lib.js
+++ codes/lib.js
@@ -1,14 +1,21 @@
 // HOF --> takes action function as argument
 function forEach(elems, action) {
-    for(elem in elems) {
-        action(elem);
+    for(i = 0 ; i < elems.length; i++) {
+        action(elems[i]);
     }
 }
 
+// OCP
 function filter(elems, predicate) {
-
+    var result = [];
+    forEach(elems, function(elem) {
+        if(predicate(elem)) {
+            result.push(elem);
+        }
+    })
+    return result;
 }
 
 function map(elems, transformFn) {
-    
+
 }
