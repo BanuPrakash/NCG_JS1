c7c5d2df5ed1b70c4d6331efdf98fae9bcedcbf2
Banuprakash <banuprakashc@yahoo.co.in>
2024-07-17 12:27:18 +0530

    day 6

--- contact-manager/src/App.js
+++ contact-manager/src/App.js
@@ -1,21 +1,42 @@
 import { connect } from 'react-redux';
 import './App.css';
+import { useRef } from 'react';
 
 function App(props) {
+  let nameRef = useRef();
+  let emailRef = useRef();
+
+  function clearAllContacts() {
+    props.clearContacts();
+  }
+  function addNewContact() {
+    let contact = {
+      name: nameRef.current.value,
+      email: emailRef.current.value
+    }
+    props.addContact(contact);
+  }
   return (
     <div className="App">
       {
-        props.contactlist.map(contact => <ContactView 
-          removeContact={props.removeContact} 
-          contact={contact} key={contact.email}/>)
+        props.contactlist.map(contact => <ContactView
+          removeContact={props.removeContact}
+          contact={contact} key={contact.email} />)
       }
+
+      <form>
+        <button type="button" onClick={() => clearAllContacts()}>Clear</button> <br />
+        Email : <input type="email" ref={emailRef} /> <br />
+        Name : <input type="text" ref={nameRef} /> <br />
+        <button onClick={() => addNewContact()}>Add Contact</button>
+      </form>
     </div>
   );
 }
 
 function ContactView(props) {
   return <div>
-    {props.email}, {props.name} 
+    {props.email}, {props.name}
     <button onClick={() => props.removeContact(props.email)}>Delete</button>
   </div>
 }
